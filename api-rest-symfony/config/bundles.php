860f978d77524721b71fb6cd2c5954f3a456690f
Nathaniel <natharevolution@gmail.com>
2022-08-16 16:30:48 -0400

    agreagando packetes y componente de videos

--- api-rest-symfony/config/bundles.php
+++ api-rest-symfony/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
