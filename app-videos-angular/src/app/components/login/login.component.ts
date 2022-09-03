e6fd63726a21bf035f327d821df07503ac79c241
Nathaniel <natharevolution@gmail.com>
2022-09-02 23:06:55 -0400

    agregando apartado de login (logicamente)

--- app-videos-angular/src/app/components/login/login.component.ts
+++ app-videos-angular/src/app/components/login/login.component.ts
@@ -1,17 +1,87 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { Router, ActivatedRoute, Params } from "@angular/router"
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
   public Pagetitle: string
-  constructor() {
+  public user: User
+  public identity: any
+  public status: string | any
+  public token: string | any
+
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
     this.Pagetitle = "Login"
+    this.user = new User(1, '', '', '', '', 'ROLE_USER', '')
+  }
+  ngOnInit(): void {
+    this.logout()
   }
 
-  ngOnInit(): void {
+  onSubmit(form: any) {
+    this._userService.signup(this.user).subscribe(
+      res => {
+        if (!res.status || res.status != 'error') {
+          this.status = 'success'
+          this.identity = res
+          console.log(this.identity)
+
+          // sacar el token 
+          this._userService.signup(this.user, true).subscribe(
+            res => {
+              if (!res.status || res.status != 'error') {
+                this.token = res
+                console.log(this.identity)
+                console.log(this.token)
+
+                localStorage.setItem('token', this.token)
+                localStorage.setItem('identity', JSON.stringify(this.identity))
+
+                this._router.navigate(['/inicio'])
+              }
+            },
+            error => {
+              this.status = 'error'
+              console.log(error)
+            }
+          )
+
+        } else {
+          this.status = 'error'
+        }
+      },
+      error => {
+        this.status = 'error'
+        console.log(error)
+      }
+    )
+  }
+
+
+  logout() {
+    this._route.params.subscribe(params => {
+      let sure = +params['sure']
+
+      if (sure == 1) {
+        localStorage.removeItem('identity')
+        localStorage.removeItem('token')
+
+        this.identity = null
+        this.token = null
+
+        this._router.navigate(['/inicio'])
+      }
+    })
   }
 
 }
--- app-videos-angular/src/app/services/user.service.ts
+++ app-videos-angular/src/app/services/user.service.ts
@@ -9,6 +9,8 @@ import { global } from "./global"
 @Injectable()
 export class UserService {
     public url: string
+    public identity: any
+    public token: any
 
     constructor(
         public _http: HttpClient
@@ -27,6 +29,41 @@ export class UserService {
         let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
 
         return this._http.post(this.url + 'register', params, { headers: headers })
+    }
+
+    signup(user: any, gettoken: any = null): Observable<any> {
+        if (gettoken != null) {
+            user.gettoken = true
+        }
+
+        let json = JSON.stringify(user)
+        let params = 'json=' + json
+
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+
+        return this._http.post(this.url + 'login', params, { headers: headers })
+    }
+
+    getIdentity() {
+        let identity = JSON.parse(localStorage.getItem('identity') || "{}")
+
+        if (identity && identity != 'undefined') {
+            this.identity = identity
+        } else {
+            this.identity = null
+        }
+        return this.identity
+    }
+
+    getToken() {
+        let token = localStorage.getItem('token')
+
+        if (token && token != 'undefined') {
+            this.token = token
+        } else {
+            this.token = null
+        }
+        return this.token
     }
 
 
